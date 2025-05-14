1357e43af501c27a03ed8b721e8168001d8ecffa
WesleyAraujoFernandes <wesley@viaapia.com.br>
2025-05-14 11:55:28 -0300

    Alteração na classe principal

--- src/main/java/tech/buildrun/urlshortener/UrlshortenerApplication.java
+++ src/main/java/tech/buildrun/urlshortener/UrlshortenerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlshortenerApplication {
-	// Classe principal
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortenerApplication.class, args);
 	}
